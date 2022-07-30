781d15cb837aba969d375d9b69d6b8b99006b0ab
Neeraj <neerajkumarghildiyal@gmail.com>
2022-07-30 20:06:20 +0530

    Best program written to understand why we use State instead of variable from Sidhu React video

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
 function App() {
+
+  let name = 'Neeraj'
+
+
+  function updateData(){
+
+    name = 'Jaanu' 
+    alert(name);
+  }
+  console.warn("--------------")
   return (
     <div className="App">
-      {/* <h1>Hello React!!</h1> */}
+      <h1>{name}</h1>
+
+      <button className='btn btn-primary' onClick={updateData}>Update Name</button>
     </div>
   );
 }
