4800b116bfd08980da848fc58c20f2ea1186f96a
Neeraj <neerajkumarghildiyal@gmail.com>
2022-08-09 20:54:50 +0530

    UseRef in ReactJS

--- src/RefInReactJS/Ref.js
+++ src/RefInReactJS/Ref.js
@@ -13,13 +13,14 @@ class Ref extends React.Component {
 
         // console.warn(this.inputRef)
         //ye jo hum current.value modify karwa rahe hai ye direct DOM manupulation hai 
-        console.warn(this.inputRef.current.value = '1999')
+        // console.warn(this.inputRef.current.value = '1999')
     }
 
 
         getValue(){
 
-            
+            console.warn(this.inputRef.current.value)
+            this.inputRef.current.style.color = 'gold'
         }
 
     render() {
@@ -29,6 +30,7 @@ class Ref extends React.Component {
             <div className="m-5">
                 <h1>Ref In React!!</h1>
                 <input type="text" ref={this.inputRef}/>
+                <button onClick={()=>this.getValue()}>Check Ref</button>
             </div>
         )
 
