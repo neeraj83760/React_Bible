6541f36a8c6ff5e42475deef677d4b601eed57bc
Neeraj <neerajkumarghildiyal@gmail.com>
2022-08-10 17:56:22 +0530

    ForwardRef in ReactJS

--- src/ForwardRef/Child22.js
+++ src/ForwardRef/Child22.js
@@ -1,5 +1,6 @@
 import React,{forwardRef} from 'react';
-
+// ye jo aapne App22 se as a props pass kiya hai wo aapko Child22 function ke props argument me
+// nahi ref argument me milega jo ki second argument hai Child22 function ka 
 function Child22(props, ref) {
 
     return(
@@ -10,4 +11,6 @@ function Child22(props, ref) {
     )
 }
 
+// ForwardRef is a wrapper that's which we have wrapped Child22 component with it.
 export default forwardRef(Child22);
+
