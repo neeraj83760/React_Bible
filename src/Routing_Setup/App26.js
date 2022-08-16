ae94ec1f575bf7a287f4c405191db2c184556719
Neeraj <neerajkumarghildiyal@gmail.com>
2022-08-17 00:25:07 +0530

    Routing SetUp in ReactJS

--- src/Routing_Setup/App26.js
+++ src/Routing_Setup/App26.js
@@ -1,11 +1,20 @@
+import React from 'react'; 
+import {BrowserRouter as Router, Link , Route, Routes} from 'react-router-dom'
+
 
 function App26(){
 
     return(
-        <div className="m-5">
-            <h2>Router Component!!</h2>
-            <Home />
-            <AboutUs />
+        <div>
+            {/* <h2>Router Component!!</h2> */} 
+            <Router>
+            <Link to="/home">Home Page</Link><br />
+            <Link to="/about">About Us</Link>
+                <Routes>
+                    <Route path="home" element={<Home />}></Route>
+                    <Route path="about" element={<AboutUs />}></Route>
+                </Routes>
+            </Router>
         </div>
     )
 
