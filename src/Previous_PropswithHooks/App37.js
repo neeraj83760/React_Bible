a28ba6489d674aaa0e895a17419eaf56b6bf69e6
Neeraj <neerajkumarghildiyal@gmail.com>
2022-08-20 20:13:32 +0530

    Previous Props with Hooks

--- src/Previous_PropswithHooks/App37.js
+++ src/Previous_PropswithHooks/App37.js
@@ -1,10 +1,19 @@
-import React from 'react';  
+import React,{useState} from 'react';
+import User37 from './User37'  
 
 function App37(){
+const [count, setCount] = useState(0)
 
+function updateCounter(){
+
+    setCount(Math.floor(Math.random()*10) )
+
+}
     return(
-        <div className="">
-         <h1>Previous Props !!!</h1>   
+        <div className="m-5">
+         {/* <h1>{count}</h1> */}
+         <User37 count ={count}/>
+         <button className="btn btn-danger" onClick={updateCounter}>Update Counter</button>  
         </div>
     )
 }
--- src/Previous_PropswithHooks/User37.js
+++ src/Previous_PropswithHooks/User37.js
@@ -1,12 +1,26 @@
-import React from 'react';  
+import React from 'react';
+import {useEffect,useRef} from 'react';  
 
-function User37(){
+function User37(props){
+    // useRef last waali value ko uthayega DOM se .. to current value update
+    // hone se pehle hi ye useEffect chal jaayega aur ye apni last value ko store
+    // kar lega aur baad me count ke andar new value aayegi 
 
+    const lastVal = useRef();
+    
+    useEffect(()=>{
+       // current useEffect ki predefined property
+        lastVal.current = props.count;
+    })
+    
+    const previousProps = lastVal.current
     return(
         <div className="">
-         <h1>User Component!!!</h1>   
+         <h1>Current Val  :   {props.count}</h1>
+         <h2>Previous Val : {previousProps}</h2>   
         </div>
     )
 }
 
 export default User37;
+
